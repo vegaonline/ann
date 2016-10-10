85c5a7c923c756d4107d90af7808db47e011d2e4
Abhijit Bhattacharyya <abhihere@gmail.com>
2016-10-11 02:05:22 +0530

    modify...

--- src/network.cpp
+++ src/network.cpp
@@ -96,9 +96,178 @@ void ANNetwork::randomize_weights(unsigned int rseed) {
       for (int i = 0; i < layers[l]->neurons[n]->get_input_links_number(); i++) {
 	wt = 0xFFF & rand();
 	wt -= 0x800;
-	layers[l]->neurons[n]->inputs[i]->wt = (float)w / 2048.0f;
+	layers[l]->neurons[n]->inputs[i]->wt = (float)wt / 2048.0f;
       }
     }
   }
 }
 	  
+//////////////  init links /////////////////////
+void network::init_links(const float* avec, const float* mvec, int ifunc, int hfunc) {
+  NLayer* plr;        // current layer
+  NLayer* pprevlr;    // previous layer
+  neuron* pnrn;       // neuron pointer
+
+  int l = 0;
+
+  //////////// input layer  /////////////
+  plr = layers[l++];
+  swprintf(plr->layer_name, L"input layer");
+  for (int n = 0; n < plr->get_neurons_number(); n++) {
+    pnrn = plr->neurons[n];
+    pnrn->function = ifunc;
+    pnrn->add_input();       // one input link for every "input layer" neuron
+    if (avec)
+      pnrn->inputs[0]->iadd = avec[n];  // default add = 0
+    if (mvec)
+      pnrn->inputs[0]->wt = mvec[n];    // default wt = 0
+    else 
+      pnrn->inputs[0]->wt = 1.0f;       //default wt = 0 for every layer neuron
+  }
+
+  /////////// hidden layer :: 1 bias ////////////
+    for (int i = 0; i < m_layers_number - 2; i++) { // 1 input [l-2 hidden] 1output
+      pprevlr = plr;
+      plr = layers[l++];
+      swprintf(plr->layer_name, L"hidden layer %d", i + 1);
+
+      for (int n = 0; n < plr->get_neurons_number(); n++) {
+	pnrn = plr->neurons[n];
+	pnrn->function = hfunc;
+	pnrn->add_bias();
+
+	for (int m = 0; m < pprevlr->get_neurons_number(); m++)
+	  pnrn->add_input(pprevlr->neurons[m]);
+      }
+    }
+
+    //////////// output layer :: 1 bias ///////////////////
+    pprevlr = plr;
+    plr = layers[l++];
+    swprintf(plr->layer_name, L"output layer");
+ 
+    for (int n = 0; n < plr->get_neurons_number(); n++) {
+      pnrn = plr->neurons[n];
+      pnrn->function = hfunc;
+      pnrn->add_bias();
+
+      for (int m = 0; m < pprevlr->get_neurons_number(); m++) 
+	pnrn->add_input(pprevlr->neurons[m]);
+    }
+}
+
+
+
+///////////// backpropagation training /////////////////////
+void network::backprop_run(const float* dsrdvec) {
+  float nrule = m_nrule;   // learning rule
+  float alpha = m_alpha;   // momentum
+  float delta, dw, oval;
+
+
+// get deltas for " output layer"
+  for (int n = 0; n < layers[m_layers_number - 1]->get_neurons_number(); n++) {
+    oval = layers[m_layers_number - 1]->neurons[n]->oval;
+    layers[m_layers_number - 1]->neurons[n]->delta = oval * (1.0f - oval) * (dsrdvec[n] - oval);
+  }
+
+  // get deltas for hidden layers
+  for (int l = m_layers_number - 2; l > 0; l--) {
+    for (int n = 0; n < layers[l]->get_neurons_number(); n++) {
+      delta = 0.0f;
+      for (int i = 0; i < layers[l]->neurons[n]->get_output_links_number(); i++)
+	      delta+=layers[l]->neurons[n]->outputs[i]->wt * layers[l]->neurons[n]->outputs[i]->pinput_neuron->delta;
+	    oval = layers[l]->neurons[n]->oval;
+	    layers[l]->neurons[n]->delta = oval * (1 - oval) * delta;
+    }
+  }
+
+
+///////// correct weights for every layer //////////////////
+    for (int l = 1; l < m_layers_number; l++) {
+      for (int n = 0; n < layers[l]->get_neurons_number(); n++) {
+	for (int i = 0; i < layers[l]->neurons[n]->get_input_links_number(); i++) {
+	  dw = nrule * layers[l]->neurons[n]->inputs[i]->ival * layers[l]->neurons[n]->delta;
+	  dw += alpha * layers[l]->neurons[n]->inputs[i]->dwprv;
+	  layers[l]->neurons[n]->inputs[i]->dwprv = dw;
+	  layers[l]->neurons[n]->inputs[i]->wt += dw;  // correct weight
+	}
+      }
+    }
+}
+
+
+bool network::train(const float* ivec, float* ovec, const float* dsrdvec, float error) { // 0.0 - 1.0 learning
+  float dst = 0.0f;
+  classify(ivec, ovec); // run network, compute inputs to output
+  for (int n = 0; n < layers[m_layers_number - 1]->get_neurons_number(); n++) {
+    dst = fabs(ovec[n] - dsrdvec[n]);
+    if (dst > error) break;
+  }
+  if (dst > error) {
+    backprop_run(dsrdvec);  // it was trained
+    return true;
+  } else                     // it wasnt trained
+    return false;
+}
+
+      
+/////////// run network /////////////
+void network::classify(const float* ivec, float* ovec) {
+  // input layer
+  for (int i = 0; i < layers[0]->get_neurons_number(); i++) {
+    layers[0]->neurons[i]->inputs[0]->ival = ivec[i];
+    layers[0]->neurons[i]->input_fire();
+  }
+
+  // hidden and output layers
+  for (int l =1; l < m_layers_number; l++)
+    for (int n = 0; n < layers[l]->get_neurons_number(); n++)
+      layers[l]->neurons[n]->fire();
+  
+  // produce ANN output
+  network_output(ovec);
+}
+
+////////// network out ////////////
+void network::network_output(float* ovec) const{
+  for (int n = 0; n < layers[m_layers_number - 1]->get_neurons_number(); n++)
+    ovec[n] = layers[m_layers_number - 1]->neurons[n]->oval;
+}
+
+//////////////// save network configuration /////////
+bool network::save(const wchar_t* fname) const {
+  FILE* fp = _wfopen(fname, L"wt");
+ if (fp) {
+   fwprintf(fp, L"%d\n", m_layers_number);
+   for (int l = 0; l < m_layers_number; l++)
+     fwprintf(fp, L"%d ", layers[l]->get_neurons_number());
+   fwprintf(fp, L"\n\n");
+
+
+   //input hidden/output layer neuron function 0-linear,1-sigmoidal
+   fwprintf(fp, L"%d\n%d\n\n", layers[0]->neurons[0]->function, layers[1]->neurons[0]->function);
+
+   for (int n = 0; n < layers[0]->get_neurons_number(); n++) {
+     fwprintf(fp, L"%f ", layers[0]->neurons[n]->inputs[0]->iadd); //add term  0.0 default
+     fwprintf(fp, L"%f\n", layers[0]->neurons[n]->inputs[0]->wt);    //multiply term  1.0 default
+   }
+   fwprintf(fp, L"\n");
+
+
+   for (int l = 1; l < m_layers_number; l++) {  //save all weights except input layer
+     for (int n = 0; n < layers[l]->get_neurons_number(); n++) {
+       for (int i = 0; i < layers[l]->neurons[n]->get_input_links_number(); i++)
+	 fwprintf(fp, L"%f\n", layers[l]->neurons[n]->inputs[i]->wt);
+     }
+     fwprintf(fp, L"\n");
+   }
+   fclose(fp);
+   return true;
+ } else
+   return false; 
+}
+
+	  
+
+	
